7e91c28f965d79e75dfd98a8f0d0b0bc4dea76b4
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-10 10:13:12 -0600

    refactor: clean up login mutation

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,14 @@ const typeDefs = gql`
     username: String!
   }
   type AuthUser {
-    id: ID!
-    username: String!
+    user: User!
     token: String!
   }
   type Query {
     user: User
   }
   type Mutation {
-    login: AuthUser
+    login(username: String!, password: String!): AuthUser
   }
 `
 
@@ -33,11 +32,12 @@ const resolvers = {
     }),
   },
   Mutation: {
-    login: () => {
+    login: (parent, { username, password }, { res }) => {
       // would add user verification here
-      const token = jwt.sign({ username: req.body.username }, "topSecret", {
+      const token = jwt.sign({ username: username }, "topSecret", {
         expiresIn: "15m",
       })
+      const refreshToken = uuidv4()
       res.cookie("refreshToken", refreshToken, {
         maxAge: 60 * 24 * 30 * 60 * 1000, // convert 30 days from minute to milliseconds
         httpOnly: true,
@@ -45,19 +45,15 @@ const resolvers = {
       })
 
       return {
-        id: 1,
-        username: "art",
+        user: {
+          id: 1,
+          username: "art",
+        },
         token,
       }
     },
   },
 }
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req, res }) => ({ req, res }),
-})
 
 const app = express()
 app.use(
@@ -66,36 +62,8 @@ app.use(
     credentials: true,
   }),
 )
-app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("tiny"))
-
-const refreshTokens = []
-
-// app.post("/login", (req, res) => {
-//   if (req.body.username !== "art" && req.body.username !== "vandelay") {
-//     return res.send({
-//       token: "",
-//     })
-//   }
-//   const token = jwt.sign({ username: req.body.username }, "topSecret", {
-//     expiresIn: "15m",
-//   })
-//   const refreshToken = uuidv4()
-//   refreshTokens.push(refreshToken)
-//   res.cookie("refreshToken", refreshToken, {
-//     maxAge: 60 * 24 * 30 * 60 * 1000, // convert 30 days from minute to milliseconds
-//     httpOnly: true,
-//     secure: false,
-//   })
-//   return res.send({
-//     token,
-//     user: {
-//       firstName: "Art",
-//       lastName: "Vandelay",
-//     },
-//   })
-// })
 
 app.post("/refresh_token", (req, res) => {
   const refToken = req.cookies.refreshToken
@@ -104,17 +72,16 @@ app.post("/refresh_token", (req, res) => {
       token: "",
     })
   }
-  const refreshTokenVerified = refreshTokens.includes(refToken)
-  if (!refreshTokenVerified) {
-    return res.send({
-      token: "",
-    })
-  }
+  // const refreshTokenVerified = refreshTokens.includes(refToken)
+  // if (!refreshTokenVerified) {
+  //   return res.send({
+  //     token: "",
+  //   })
+  // }
   const token = jwt.sign({ username: req.body.username }, "topSecret", {
     expiresIn: "15m",
   })
   const refreshToken = uuidv4()
-  refreshTokens.push(refreshToken)
   res.cookie("refreshToken", refreshToken, {
     maxAge: 60 * 24 * 30 * 60 * 1000, // convert 30 days from minute to milliseconds
     httpOnly: true,
@@ -133,7 +100,13 @@ app.post("/logout", (req, res) => {
   return res.send("OK")
 })
 
-server.applyMiddleware({ app })
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({ req, res }),
+})
+
+apolloServer.applyMiddleware({ app, cors: false })
 
 const port = process.env.PORT || 3001
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
