5f2b220933a2da9d55bfb65177dd169121cced8d
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-14 15:11:18 -0600

    refactor: remove the use of custom hook, replace with two useEffects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React, { useCallback } from "react"
+import React, { useEffect, useCallback } from "react"
 import { ApolloProvider } from "@apollo/react-hooks"
 import { ApolloClient } from "apollo-client"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { createHttpLink } from "apollo-link-http"
 import { setContext } from "apollo-link-context"
-// import jwtDecode from "jwt-decode"
+import jwtDecode from "jwt-decode"
 import { useAuthStore } from "./AuthContext"
-import useInterval from "./useInterval"
+// import useInterval from "./useInterval"
 import "./App.css"
 import Routes from "./Routes"
 
@@ -31,69 +31,51 @@ const clientCreator = token => {
   })
 }
 
+const getTokenIntervalDelay = token => {
+  const decodedToken = jwtDecode(token)
+  const currentTime = new Date(Date.now())
+  const jwtTime = new Date(decodedToken.exp * 1000)
+  const timeDiffInMins = (jwtTime - currentTime) / 60000
+  // all this to say we want the delay to be two minutes before the JWT expires
+  return (timeDiffInMins - 2) * 60 * 1000
+}
+
 const App = () => {
-  const [{ token }, dispatch] = useAuthStore()
+  const [{ token, isAuthenticated }, dispatch] = useAuthStore()
 
-  useInterval(
-    useCallback(async () => {
-      try {
-        const res = await fetch(
-          `${process.env.REACT_APP_API_SERVER}/refresh_token`,
-          {
-            method: "POST",
-            credentials: "include",
-          },
-        )
-        const json = await res.json()
-        dispatch({
-          type: "UPDATE_TOKEN",
-          payload: {
-            token: json.token,
-          },
-        })
-      } catch (error) {
-        console.error(error)
-      }
-    }, [dispatch]),
-    60000,
-    token,
-  )
+  const fetchRefreshToken = useCallback(async () => {
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_API_SERVER}/refresh_token`,
+        {
+          method: "POST",
+          credentials: "include",
+        },
+      )
+      const json = await res.json()
+      dispatch({
+        type: "UPDATE_TOKEN",
+        payload: {
+          token: json.token,
+        },
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }, [dispatch])
 
-  // useEffect(() => {
-  //   let interval
-  //   const fetchRefreshToken = async () => {
-  //     try {
-  //       const res = await fetch(
-  //         `${process.env.REACT_APP_API_SERVER}/refresh_token`,
-  //         {
-  //           method: "POST",
-  //           credentials: "include",
-  //         },
-  //       )
-  //       const json = await res.json()
-  //       dispatch({
-  //         type: "UPDATE_TOKEN",
-  //         payload: {
-  //           token: json.token,
-  //         },
-  //       })
-  //     } catch (error) {
-  //       console.error(error)
-  //     }
-  //   }
+  useEffect(() => {
+    fetchRefreshToken()
+  }, [fetchRefreshToken])
 
-  //   // need to fetch on mount
-  //   // if token is in state, then set interval to fetch refresh token
-  //   if (token !== "") {
-  //     interval = setInterval(() => {
-  //       fetchRefreshToken()
-  //     }, 60000) // check every minute
-  //   } else {
-  //     fetchRefreshToken()
-  //   }
-
-  //   return () => clearInterval(interval)
-  // }, [dispatch, token])
+  useEffect(() => {
+    if (isAuthenticated) {
+      const interval = setInterval(() => {
+        fetchRefreshToken()
+      }, getTokenIntervalDelay(token))
+      return () => clearInterval(interval)
+    }
+  }, [fetchRefreshToken, isAuthenticated, token])
 
   return (
     <ApolloProvider client={clientCreator(token)}>
